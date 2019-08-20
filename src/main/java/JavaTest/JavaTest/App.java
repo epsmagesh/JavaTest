9e054ec8631d9301dacac11aed3a325d3234e3fb
epsmagesh <54285235+epsmagesh@users.noreply.github.com>
2019-08-19 20:43:43 -0700

    123 codes

---
+++ src/main/java/JavaTest/JavaTest/App.java
@@ -0,0 +1,62 @@
+package JavaTest.JavaTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        for(int i=1;i<=6;i++)
+        {
+        	for(int j=1;j<=i;j++)
+        	{
+        		 System.out.print(j);
+        	}
+        	System.out.println();
+        }
+       
+        
+        
+        for(int a=1;a<=4;a++)
+        {
+        	if(a==2||a==3)
+        	{
+        		System.out.println("*  *");
+        	}
+        	else
+        	System.out.println("****");
+        }
+        
+        
+        
+     for(int k=1;k<=4;k++)
+     {
+    	 switch(k) 
+    	 {
+    		 case 1:
+    			 System.out.println("****");
+    			 break;
+    			 
+    		 case 2:
+    			 System.out.println("*  *");
+    			 break;
+    			 
+    		 case 3:
+    			 System.out.println("*  *");
+    			 break;
+    			 
+    		 case 4:
+    			 System.out.println("****");
+    			 break;
+    			 
+    	 }
+    	 
+    		 
+    	 
+     }
+      
+    }
+}
